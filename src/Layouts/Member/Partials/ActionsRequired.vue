210a6b65cd8c96d96270624b6119ae835936bc5c
rob <dev@mexion.com>
2021-11-06 13:06:24 +0100

    Team invites

---
+++ src/Layouts/Member/Partials/ActionsRequired.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="confirmableActions">
+
+    <div v-for="action in confirmableActions" :class="'alert alert-info'"
+         style="overflow-wrap: break-word">
+      <div class="d-flex justify-content-between">
+        <div>
+          <strong>{{ action.title }}</strong>
+        </div>
+        <div>
+          <a :href="action.ctaUrl">
+            <button class="btn btn-primary">{{ action.ctaLabel }}</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      confirmableActions: this.$page.props.shared.confirmableActions,
+    };
+  },
+};
+</script>
+
