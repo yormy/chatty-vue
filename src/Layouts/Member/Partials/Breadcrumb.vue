6b88c16add764861c124b00343ec0b8f44b03d80
rob <dev@mexion.com>
2021-11-06 13:14:10 +0100

    add breadcrumbs

--- src/Layouts/Member/Partials/Breadcrumb.vue
+++ src/Layouts/Member/Partials/Breadcrumb.vue
@@ -1,32 +1,53 @@
 <template>
-  <div class="row d-none d-sm-flex">
-    <div class="col-12 d-flex pb-4 pt-2">
-      <h4 class="page-title">Dashbaord</h4>
-      <div class="nav d-none d-lg-flex dashboard-quick-link">
-        <div class="nav-item">
-          <a class="nav-link" href="blank-page.html"
+  <div>
+    <div class="row d-none d-sm-flex">
+      <div class="col-12 d-flex pb-4 pt-2">
+        <h4 class="page-title">Dashbaord</h4>
+
+        <div class="nav d-none d-lg-flex dashboard-quick-link">
+          <div class="nav-item">
+            <a class="nav-link" href="blank-page.html"
             ><i class="mdi mdi-content-save mr-2"></i>Save Report</a
-          >
-        </div>
-        <div class="nav-item">
-          <a class="nav-link" href="blank-page.html"
+            >
+          </div>
+          <div class="nav-item">
+            <a class="nav-link" href="blank-page.html"
             ><i class="mdi mdi-file-pdf mr-2"></i>Export to PDF</a
-          >
-        </div>
-        <div class="nav-item">
-          <a class="nav-link" href="blank-page.html"
+            >
+          </div>
+          <div class="nav-item">
+            <a class="nav-link" href="blank-page.html"
             ><i class="mdi mdi-email mr-2"></i>Send to Email</a
-          >
+            >
+          </div>
         </div>
-      </div>
-      <div class="btn-wrapper ml-auto">
-        <button class="btn btn-outline-secondary btn-rounded mr-3" type="button">Manage</button>
-        <button class="btn btn-primary btn-rounded" type="button">Add Customer</button>
+        <div class="btn-wrapper ml-auto">
+          <button class="btn btn-outline-secondary btn-rounded mr-3" type="button">Manage</button>
+          <button class="btn btn-primary btn-rounded" type="button">Add Customer</button>
+        </div>
       </div>
     </div>
+
+    <breadcrumbs :breadcrumbs="breadcrumbs"></breadcrumbs>
   </div>
 </template>
 
 <script>
-export default {};
+import {Breadcrumbs} from "bedrock-core-vue";
+
+export default {
+  components: {
+    Breadcrumbs,
+  },
+
+  props: {
+    breadcrumbs: {
+      type: Array,
+      required: false,
+      default() {
+        return []
+      }
+    },
+  },
+};
 </script>
